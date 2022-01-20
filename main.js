b8f14571725e456ecde775dae1f7bb2c4297edbf
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-01-20 23:04:40 +0100

    Creata api e passato dati nell'html

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 const app = new Vue({
     el: "#app",
     data: {
-        
-    }
+        DATA: []
+    },
+
+    created() {
+        axios.get('http://localhost/php-ajax-dischi/backend.php')
+        .then((response) => {
+            this.DATA = response.data;
+        })
+    },
+
+    // methods: {
+    //     metodoRicerca(research) {
+    //         this.albumsFiltered = this.albums.filter((elem) => {
+    //             return elem.title.toLowerCase().includes(research.toLowerCase());
+    //         });
+    //     }
+    // }
 })
